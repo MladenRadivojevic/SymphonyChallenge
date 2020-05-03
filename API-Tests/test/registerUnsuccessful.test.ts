30e04c36f94bfa3dfb66836609ef1e73e6f2b5b7
MladenRadivojevic <mladenradivojevic389@gmail.com>
2020-05-03 11:41:43 +0200

    Initial commit, API Tests

--- API-Tests/test/registerUnsuccessful.test.ts
+++ API-Tests/test/registerUnsuccessful.test.ts
@@ -5,15 +5,19 @@ import faker from 'faker';
 describe('Unsuccessful registraion', () => {
     it('Registration shoul be unsuccessful', async () => {
 
+        const generatedEmail =  faker.internet.email(); // Generating fake data ( email and password ), due the unsuccesful registration is tested 
+        const generatedPassword = faker.internet.password();
     try
     {
-        await axios.post(ReqresApi.registerUnsuccessful, new RegisterDTO({email: faker.internet.email(), password: faker.internet.password()}));
+        await axios.post(ReqresApi.registerUnsuccessful, new RegisterDTO({email: generatedEmail, password: generatedPassword}));
     }
     catch(e){
+        
+        console.log(`Data used for testing: Email: ${generatedEmail}, Password: ${generatedPassword}`);
         expect(e.response.status).toBe(400);
         expect(e.response.data.error).toBe("Note: Only defined users succeed registration");
     }
-    
+
   });
 });
   
